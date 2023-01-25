758614ae0cf3ddcaec56787400bd6652f967c64a
Andrei Rusu <andrei.rusu@beatfactor.net>
2023-01-25 14:02:57 +0100

    updated package versions

--- nightwatch.conf.cjs
+++ nightwatch.conf.cjs
@@ -13,7 +13,7 @@ module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
   src_folders: [],
-  plugins: ['vite-plugin-nightwatch'],
+  plugins: ['@nightwatch/vue'],
   page_objects_path: ['node_modules/nightwatch/examples/pages/'],
   custom_commands_path: ['node_modules/nightwatch/examples/custom-commands/'],
   custom_assertions_path: '',
--- test/component/todoFormTest.js
+++ test/component/todoFormTest.js
@@ -3,7 +3,7 @@ describe('Component test for the TodoForm', function() {
   browser.baseUrl = browser.baseUrl || 'http://localhost:3000';
 
   it('render and test the component', async function(browser) {
-    const component = await browser.mountVueComponent('/src/components/ToDoForm.vue');
+    const component = await browser.mountComponent('/src/components/ToDoForm.vue');
     browser.expect(component).to.be.visible;
 
     const labelEl = await component.find('label[for="new-todo-input"]');
