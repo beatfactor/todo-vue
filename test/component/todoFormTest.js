dd2e95b7655a5e8b42773eb4080613d594a4696c
Paul Hammant <paul@hammant.org>
2023-04-26 20:38:05 +0100

    with_test_harness

--- test/component/todoFormTest.js
+++ test/component/todoFormTest.js
@@ -1,13 +1,28 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 describe('Component test for the TodoForm', function() {
 
   browser.baseUrl = browser.baseUrl || 'http://localhost:3000';
 
   it('render and test the component', async function(browser) {
-    const component = await browser.mountComponent('/src/components/ToDoForm.vue');
+    const component = await browser.mountComponent('/test/component/ToDoForm_TestHarness.vue');
     browser.expect(component).to.be.visible;
 
-    const labelEl = await component.find('label[for="new-todo-input"]');
-    expect(labelEl).text.toContain('What needs to be done?');
+    expect(await component.find('label[for="new-todo-input"]'))
+        .text.toContain('What needs to be done?');
+
+    expect(await component.find('#testHarnessOutput')).text.toBe('todo-added:', );
+
+    //await delay(10000);
+    browser.clearValue('#new-todo-input')
+        .sendKeys('#new-todo-input', "abc")
+        .click("button")
+        .clearValue('#new-todo-input')
+        .sendKeys('#new-todo-input', "def")
+        .click("button");
+
+    expect(await component.find('#testHarnessOutput')).text.toBe('todo-added: abcdef', );
+
 
   });
 
